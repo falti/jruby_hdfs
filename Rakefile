5cd757c276072eb5886ec3553f903ab8c35feacc
Frank Falkenberg <faltibrain@gmail.com>
2011-10-13 00:47:33 +0200

    converted tabs to spaces

--- Rakefile
+++ Rakefile
@@ -4,8 +4,8 @@ task :default => [:test]
 
 desc "Run tests"
 task :test do
-	Rake::TestTask.new(:test) do |t|
-	  t.test_files = FileList['test/*_test.rb']
-	  t.libs << 'test' << 'lib'
-	end
+  Rake::TestTask.new(:test) do |t|
+    t.test_files = FileList['test/*_test.rb']
+    t.libs << 'test' << 'lib'
+  end
 end
--- test/exploration_test.rb
+++ test/exploration_test.rb
@@ -3,9 +3,9 @@ require "test/unit"
 require "hdfs"
 
 class TestExploration < Test::Unit::TestCase
-	def test_file_new
-		assert_raise(Errno::ENOENT) { f = Hdfs::File.new("xxx") }
-		assert_raise(Errno::ENOENT) { f = Hdfs::File.new(".") }
-		
-	end
+  def test_file_new
+    assert_raise(Errno::ENOENT) { f = Hdfs::File.new("xxx") }
+    assert_raise(Errno::ENOENT) { f = Hdfs::File.new(".") }
+    
+  end
 end
