92ea34c2092cece68ea54475b3a667d73992f65a
Frank Falkenberg <faltibrain@gmail.com>
2011-10-13 07:31:12 +0200

    switched to minitest

--- test/exploration_test.rb
+++ test/exploration_test.rb
@@ -1,11 +1,13 @@
-require "test/unit"
+require 'rubygems'
+gem 'minitest'
+require 'minitest/autorun'
 
 require "hdfs"
 
-class TestExploration < Test::Unit::TestCase
+class TestExploration < MiniTest::Unit::TestCase
   def test_file_new
-    assert_raise(Errno::ENOENT) { f = Hdfs::File.new("xxx") }
-    assert_raise(Errno::ENOENT) { f = Hdfs::File.new(".") }
+    assert_raises(Errno::ENOENT) { f = Hdfs::File.new("xxx") }
+    assert_raises(Errno::ENOENT) { f = Hdfs::File.new(".") }
     
   end
 end
